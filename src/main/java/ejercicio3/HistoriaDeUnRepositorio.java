59c579c1a1b108587abc40b7b699ed0feeed7a84
Rafa <rtiendagonzalez@gmail.com>
2021-10-19 16:27:12 +0200

    Inicio de esta historia
    Inicio de esta historia

---
+++ src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
@@ -0,0 +1,13 @@
+package ejercicio3;
+
+public class HistoriaDeUnRepositorio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hace un buen dia para echarse una siesta");
+		
+			
+	}
+
+}
