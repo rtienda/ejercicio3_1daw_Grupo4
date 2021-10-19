0b301ded108b0694cbfe49f827fe951a05d5f4eb
Rafa <rtiendagonzalez@gmail.com>
2021-10-19 17:05:15 +0200

    seguimos

--- src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
+++ src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
@@ -9,7 +9,8 @@ public class HistoriaDeUnRepositorio {
 		System.out.println("Pero me tome un te y sali con la bici"); 
 		System.out.println("Cuando estaba saliendo me cai por una alcantarilla");
 		System.out.println("Y se me rompio el movil"); 
-		
+		System.out.println("Me rompi la rodilla por 4 partes y dios que dolor");
+
 			
 	}
 
