6ef30a9fe0f8571e3d0cacb34554bda22d84515d
albaruiz89 <albaruizmora@gmail.com>
2021-10-19 17:25:44 +0200

    Update HistoriaDeUnRepositorio.java

--- src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
+++ src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
@@ -9,9 +9,7 @@ public class HistoriaDeUnRepositorio {
 		System.out.println("Pero me tome un te y sali con la bici"); 
 		System.out.println("Cuando estaba saliendo me cai por una alcantarilla");
 		System.out.println("Y se me rompio el movil"); 
-		System.out.println("Me rompi la rodilla por 4 partes y dios que dolor");
-		System.out.println("Despues un gitano me robo la cartera"); 
-
+		System.out.println("Despues me robaron la cartera"); 
 		System.out.println("R:Me rompi la rodilla por 4 partes y dios que dolor");
 		System.out.println("R:Grite muchisimo pero nadie me oia");
 		System.out.println("R:Estaba muy nervioso y empece a llorar del dolor");
