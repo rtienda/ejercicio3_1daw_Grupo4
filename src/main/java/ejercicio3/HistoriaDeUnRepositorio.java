73e69268c512722a8ed304f95a13233d34e0857d
DidiplayerAC <1810dan@gmail.com>
2021-10-19 17:13:00 +0200

    frase

--- src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
+++ src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
@@ -10,11 +10,13 @@ public class HistoriaDeUnRepositorio {
 		System.out.println("Cuando estaba saliendo me cai por una alcantarilla");
 		System.out.println("Y se me rompio el movil"); 
 		System.out.println("Me rompi la rodilla por 4 partes y dios que dolor");
-		System.out.println("Despues un gitano me robo la cartera"); 
+		System.out.println("Despues un hombre me robo la cartera"); 
 
 		System.out.println("R:Me rompi la rodilla por 4 partes y dios que dolor");
 		System.out.println("R:Grite muchisimo pero nadie me oia");
 		System.out.println("R:Estaba muy nervioso y empece a llorar del dolor");
+		System.out.println("R:Vino la ambulancia, pero habia mucho atasco");
+
 			
 	}
 
