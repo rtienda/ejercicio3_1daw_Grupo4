dfd88ae5dc242c3f70cbd890227bbaaa9f61651c
victoroliveiramarquez <v13.oliveira.m@gmail.com>
2021-10-19 16:47:33 +0200

    continuacion tercera
    vamos por la tercera parte

--- src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
+++ src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
@@ -7,6 +7,7 @@ public class HistoriaDeUnRepositorio {
 
 		System.out.println("Hace un buen dia para echarse una siesta");
 		System.out.println("Pero me tome un te y sali con la bici"); 
+		System.out.println("Cuando estaba saliendo me cai por una alcantarilla");
 			
 	}
 
