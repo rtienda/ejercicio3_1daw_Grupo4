ea6f5f7e0d5287b92a85b43ceffb608300254755
Rafa <rtiendagonzalez@gmail.com>
2021-10-19 17:07:46 +0200

    nombres

--- src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
+++ src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
@@ -5,11 +5,11 @@ public class HistoriaDeUnRepositorio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hace un buen dia para echarse una siesta");
+		System.out.println("R:Hace un buen dia para echarse una siesta");
 		System.out.println("Pero me tome un te y sali con la bici"); 
 		System.out.println("Cuando estaba saliendo me cai por una alcantarilla");
 		System.out.println("Y se me rompio el movil"); 
-		System.out.println("Me rompi la rodilla por 4 partes y dios que dolor");
+		System.out.println("R:Me rompi la rodilla por 4 partes y dios que dolor");
 
 			
 	}
