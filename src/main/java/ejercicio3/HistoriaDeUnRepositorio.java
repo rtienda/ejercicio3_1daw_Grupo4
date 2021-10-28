2bbd41eabc450f63c7cb81adad903a0437a3477c
rtienda <92530217+rtienda@users.noreply.github.com>
2021-10-28 17:46:48 +0200

    Clase con codigo

--- src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
+++ src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
@@ -1,30 +1,99 @@
 package ejercicio3;
 
+import java.util.*;
+
 public class HistoriaDeUnRepositorio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner texto = new Scanner(System.in);
+		
 		System.out.println("R:Hace un buen dia para echarse una siesta");
 		System.out.println("Pero me tome un te y sali con la bici"); 
-		
 		System.out.println("Cuando estaba saliendo me cai por una alcantarilla");
 		System.out.println("Y se me rompio el movil"); 
 		System.out.println("Despues un hombre me robo la cartera");
 		System.out.println("Me rompi la rodilla por 4 partes y dios que dolor");
 		System.out.println("una rodilla que ya me habia operado el aï¿½o pasado");
-
 		System.out.println("R:Me rompi la rodilla por 4 partes y dios que dolor");
-
-
-
-
 		System.out.println("R:Grite muchisimo pero nadie me oia");
 		System.out.println("R:Estaba muy nervioso y empece a llorar del dolor");
 		System.out.println("Me rompi la rodilla por 4 partes y dios que dolor");
 		System.out.println("Fui a urgencias y alli me encontre a Roger");
-
+		System.out.println("Te gusto la historia?");
+		System.out.println("SI o NO");
+		String respuesta= texto.nextLine();
+		
+		if(respuesta.equalsIgnoreCase("Si")) {
 			
+			System.out.println("Nos alegramos que te haya gustado :3");
+			
+		}else if(respuesta.equalsIgnoreCase("No")){
+			System.out.println("Pues es lo que hay crack.");
+		}else {
+			System.out.println("Mira no te entiendo XD");
+		}
+	
+		/*// + es un operador binario porque tiene un operando
+		//a la izda y otro a la dcha
+		float suma=4+5; 
+		
+		suma--; //Este es unario 
+		System.out.println("suma vale: "+suma);
+		float cuenta = 5/(suma+7-4)*2f;
+		System.out.println(cuenta);
+		
+		// EPOCH
+		long timestamp=1635434265;
+		float segundosEnUnAño=60*60*24*365.2425f;
+		System.out.println("años desde 1970: "+
+		(timestamp/segundosEnUnAño));
+		System.out.println("segundos desde el 1 de enero de 2021: "+
+		(long)(timestamp%segundosEnUnAño));
+		long segundosEnUnDia=60*60*24;
+		System.out.println("dias desde el 1 de enero de 2021:"+
+		((timestamp%segundosEnUnAño)/segundosEnUnDia));
+		
+	}asdSystem.out.println("segundos desde el 1 de enero de 2021: "+
+		(long)(timestamp%segundosEnUnAño));
+		long segundosEnUnDia=60*60*24;
+		System.out.println("dias desde el 1 de enero de 2021:"+
+		((timestamp%segundosEnUnAño)/segundosEnUnDia));package paqueteuno;
+
+import java.util.Scanner;
+
+
+https://demo.firepad.io/#1daw      
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// + es un operador binario porque tiene un operando
+		//a la izda y otro a la dcha
+		float suma=4+5; 
+		
+		suma--; //Este es unario 
+		System.out.println("suma vale: "+suma);
+		float cuenta = 5/(suma+7-4)*2f;
+		System.out.println(cuenta);
+		
+		// EPOCH
+		long timestamp=1635434265;
+		float segundosEnUnAño=60*60*24*365.2425f;
+		System.out.println("años desde 1970: "+
+		(timestamp/segundosEnUnAño));
+		System.out.println("segundos desde el 1 de enero de 2021: "+
+		(long)(timestamp%segundosEnUnAño));
+		long segundosEnUnDia=60*60*24;
+		System.out.println("dias desde el 1 de enero de 2021:"+
+		((timestamp%segundosEnUnAño)/segundosEnUnDia));
+		
+	}
+
+}
+*/
+	
 	}
 
 }
