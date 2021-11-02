4d09283a82b54b3d22eba71bcacd96c00197b2d8
albaruiz89 <albaruizmora@gmail.com>
2021-11-02 16:19:41 +0100

    je ne sais pas

--- src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
+++ src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
@@ -8,15 +8,15 @@ public class HistoriaDeUnRepositorio {
 
 		
 
-		System.out.println("R:Hace un buen dia para echarse una siesta");
+		System.out.println("Hace un buen dia para echarse una siesta");
 		System.out.println("Pero me tome un te y sali con la bici");
 		System.out.println("Cuando estaba saliendo me cai por una alcantarilla");
 		System.out.println("Y se me rompio el movil");
 		System.out.println("Despues un hombre me robo la cartera");
 		System.out.println("Me rompi la rodilla por 4 partes y dios que dolor");
 		System.out.println("una rodilla que ya me habia operado el mes pasado");
-		System.out.println("R:Grite muchisimo pero nadie me oia");
-		System.out.println("R:Estaba muy nervioso y empece a llorar del dolor");
+		System.out.println("Grite muchisimo pero nadie me oia");
+		System.out.println("Estaba muy nervioso y empece a llorar del dolor");
 		System.out.println("Fui a urgencias y alli me encontre a Roger");
 		System.out.println("Pues si, era Roger Federer");
 		System.out.println("Entonces le propuse a Roger ir a cenar");
@@ -27,7 +27,8 @@ public class HistoriaDeUnRepositorio {
 		System.out.println("Sin querer se tropezo y se rompio la crisma");
 		System.out.println("Entonces aparecio Hermione Granger y dijo que podia arreglar la situacion con su giratiempo");
 		System.out.println("No estaba segura si realmente vi a Hermione o eran los efectos de los calmantes");
-		System.out.println("lo que si tenia claro es que Roger estaba en el suelo muerto");
+		System.out.println("Lo que si tenia claro es que Roger estaba en el suelo muerto");
+		System.out.println("Me estaba desangrando en urgencias cuando apareció por fin una enfermera para ayudarme, una enfermera muy guapa");
 
 
 		
