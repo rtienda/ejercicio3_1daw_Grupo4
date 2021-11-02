5d981452ab21e980f85c36a1c56929922e92f5ae
DidiplayerAC <1810dan@gmail.com>
2021-11-02 15:42:17 +0100

    NUEVA frase

--- src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
+++ src/main/java/ejercicio3/HistoriaDeUnRepositorio.java
@@ -1,13 +1,13 @@
 package ejercicio3;
 
-
+import java.util.*;
 
 public class HistoriaDeUnRepositorio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	
+		Scanner texto = new Scanner(System.in);
 
 		System.out.println("R:Hace un buen dia para echarse una siesta");
 		System.out.println("Pero me tome un te y sali con la bici");
@@ -21,9 +21,22 @@ public class HistoriaDeUnRepositorio {
 		System.out.println("R:Estaba muy nervioso y empece a llorar del dolor");
 		System.out.println("Me rompi la rodilla por 4 partes y dios que dolor");
 		System.out.println("Fui a urgencias y alli me encontre a Roger");
+		System.out.println("Roger es un tipo majo");
 
+		System.out.println("Te gusto la historia?");
+		System.out.println("SI o NO");
+		
+		String respuesta = texto.nextLine();
 
-	
+		if (respuesta.equalsIgnoreCase("Si")) {
+
+			System.out.println("Nos alegramos que te haya gustado :3");
+
+		} else if (respuesta.equalsIgnoreCase("No")) {
+			System.out.println("Pues es lo que hay crack.");
+		} else {
+			System.out.println("Mira no te entiendo XD");
+		}
 
 	}
 }
